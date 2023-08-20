a2bf8ee092ed9d7066320e55cc53f3c6bea1a30d
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-08-19 18:27:12 -0700

    more game work

--- game/game.py
+++ game/game.py
@@ -12,7 +12,7 @@ class Weapon(object):
         self.attacks_per_turn: int = attacks_per_turn
 
 class Enemy(object):
-    def __init__(self, hp: int, weak_to: set[str] = None, strong_against: set[str] = None, damage_type: str = None, drops: Dict[str, int] = None):
+    def __init__(self, name: str, hp: int, weak_to: set[str] = None, strong_against: set[str] = None, damage_type: str = None, drops: Dict[str, int] = None):
         self.hp: int = hp
         self.weak_to: set[str] = weak_to
         self.strong_against: set[str] = strong_against
@@ -29,9 +29,11 @@ class Decider(object):
         """
     def select_weapon(weapons: list[Weapon], enemies: list[Enemy]) -> Weapon:
         raise NotImplementedError()
+    def target_enemies(weapon: Weapon, enemies: dict[str, Enemy]) -> list[Enemy]:
+        raise NotImplementedError()
     
 class Decider_CLI(Decider):
-    def select_weapon(weapons: list[Weapon], enemies: list[Enemy]) -> Weapon:
+    def select_weapon(weapons: dict[str, Weapon], enemies: list[Enemy]) -> Weapon:
         print("You have the following weapons:", weapons, "and the following enemies:", enemies)
         weaponName: str = None
         while weaponName not in weapons:
@@ -40,6 +42,12 @@ class Decider_CLI(Decider):
         weapon: Weapon = [x for x in weapons if x.name == weaponName][0]
         print("You have selected", weapon)
         return weapon
+    def target_enemies(weapon: Weapon, enemies: dict[str, Enemy]) -> list[Enemy]:
+        print("You have", weapon.attacks_per_turn, "available with", weapon, ", and the following potential targets:", enemies)
+        result: list[Enemy] = []
+        remaining_attacks = weapon.attacks_per_turn
+        while remaining_attacks > 0:
+            pass
 
 class Player(object):
     # todo: genes determine these properties
@@ -49,14 +57,10 @@ class Player(object):
         # how many times the player can use a weaapon
         self.ammo: Dict[str, int] = {}
         # the weapons available to the player
-        self.weapons: list[Weapon] = []
+        self.weapons: dict[str, Weapon] = {}
         self.decider = Decider
 
-
-
-
-
-def battle(player: Player, enemies: list[Enemy]) -> None:
+def battle(player: Player, enemies: dict[str, Enemy]) -> None:
     """
     A player fights enemies until either the player is dead or all the enemies are dead.
     """
@@ -65,6 +69,6 @@ def battle(player: Player, enemies: list[Enemy]) -> None:
         # enemies each attack player once
         # player heals hp?
         pass
-    while player.hp > 0 and any([x for x in enemies if x.hp > 0]):
+    while player.hp > 0 and any([x for x in enemies.values if x.hp > 0]):
         
         pass
