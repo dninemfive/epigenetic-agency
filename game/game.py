1e1b9cfcad80420d7f6aa259fa8c3cbacba46358
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-08-20 00:23:41 -0700

    game nearly playable

--- game/game.py
+++ game/game.py
@@ -11,8 +11,12 @@ class Weapon(object):
         # how many times the player can attack per turn if they choose this weapon
         self.attacks_per_turn: int = attacks_per_turn
 
+    def has_ammo(self, ammo: dict[str, int]) -> bool:
+        return self.required_ammo is None or ammo[self.required_ammo] > 0
+
 class Enemy(object):
     def __init__(self, name: str, hp: int, weak_to: set[str] = None, strong_against: set[str] = None, damage_type: str = None, drops: Dict[str, int] = None):
+        self.name = name
         self.hp: int = hp
         self.weak_to: set[str] = weak_to
         self.strong_against: set[str] = strong_against
@@ -21,6 +25,16 @@ class Enemy(object):
 
     def Instantiate(enemydef):
         return Enemy(enemydef.hp, enemydef.weak_to, enemydef.strong_against, enemydef.damage_type, enemydef.drops)
+    
+    def take_hit(self, weapon: Weapon) -> int:
+        dmg: int = 2
+        damageType = weapon.damage_type
+        if damageType in self.weak_to:
+            dmg = 3
+        if damageType in self.strong_against:
+            dmg = 1
+        self.hp -= dmg
+        return dmg
 
 class Decider(object):
     def __init__(self):
@@ -36,18 +50,23 @@ class Decider_CLI(Decider):
     def select_weapon(weapons: dict[str, Weapon], enemies: list[Enemy]) -> Weapon:
         print("You have the following weapons:", weapons, "and the following enemies:", enemies)
         weaponName: str = None
-        while weaponName not in weapons:
+        while weaponName not in weapons.keys():
             weaponName = input()
             print("You do not have a weapon called", weaponName, ". Please select one of", weapons)
-        weapon: Weapon = [x for x in weapons if x.name == weaponName][0]
+        weapon: Weapon = weapons[weaponName]
         print("You have selected", weapon)
         return weapon
+    
     def target_enemies(weapon: Weapon, enemies: dict[str, Enemy]) -> list[Enemy]:
         print("You have", weapon.attacks_per_turn, "available with", weapon, ", and the following potential targets:", enemies)
+        availableEnemies: list[Enemy] = enemies.values()
         result: list[Enemy] = []
         remaining_attacks = weapon.attacks_per_turn
         while remaining_attacks > 0:
-            pass
+            print("You can attack the following enemies:", availableEnemies)
+            enemyName: str = None
+            while enemyName not in enemies.keys():
+                pass
 
 class Player(object):
     # todo: genes determine these properties
@@ -57,18 +76,40 @@ class Player(object):
         # how many times the player can use a weaapon
         self.ammo: Dict[str, int] = {}
         # the weapons available to the player
-        self.weapons: dict[str, Weapon] = {}
-        self.decider = Decider
+        self.weapons: dict[str, Weapon] = {
+            "knife": Weapon("knife", "blade"),
+            "gun": Weapon("gun", "bullet", "9mm", 2)
+        }
+        # the thing which decides how the player plays
+        self.decider = decider
+
+    def attack(self, enemy: Enemy, weapon: Weapon):
+        if not weapon.has_ammo(self.ammo):
+            return
+        if weapon.required_ammo is not None:
+            self.ammo[weapon.required_ammo] -= 1
+        result: int = enemy.take_hit(weapon)
+        # todo: update epigenome with damage taken
+    
+    @property
+    def usable_weapons(self):
+        result = dict()
+        for k, v in self.weapons.items():
+            if v.has_ammo(self.ammo):
+                result[k] = v
+        return result
+
 
 def battle(player: Player, enemies: dict[str, Enemy]) -> None:
     """
     A player fights enemies until either the player is dead or all the enemies are dead.
     """
     def do_turn():
-        # player selects weapon best for the situation and attacks n enemies
+        weapon: Weapon = player.decider.select_weapon(player.usable_weapons, enemies.values())
+        targets: list[Enemy] = player.decider.target_enemies(weapon, enemies.values())
+        for target in targets:
+            player.attack(target, weapon)
         # enemies each attack player once
-        # player heals hp?
-        pass
-    while player.hp > 0 and any([x for x in enemies.values if x.hp > 0]):
-        
-        pass
+    while player.hp > 0 and any([x for x in enemies.values() if x.hp > 0]):
+        do_turn()
+    # player heals hp?
