576a747f5aec5c6ef21163982c45aae7c3c7bb54
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-08-22 19:58:40 -0700

    big refactor

--- game/game.py
+++ game/game.py
@@ -24,119 +24,113 @@ def list_str(items: enumerate) -> str:
 
 # ====================================
 
-class Weapon(object):
-    def __init__(self, name: str, damage_type: str, ammo: str = None, attacks_per_turn: int = 1):
-        # the name of the weapon, used to reference it in output
-        self.name = name
-        # damage type determines whether the weapon is strong or weak against an enemy
-        self.damage_type: str = damage_type
-        # if required ammo is not None, can only be used when the player has the corresponding ammo
-        self.required_ammo: str = ammo
-        # how many times the player can attack per turn if they choose this weapon
-        self.attacks_per_turn: int = attacks_per_turn
+DamageType = str | None
+EnemyName = str
+Attack = tuple[EnemyName, DamageType]
 
-    def has_ammo(self, ammo: dict[str, int]) -> bool:
-        return self.required_ammo is None or ammo[self.required_ammo] > 0
+DAMAGE_TYPE_BONUSES: dict[DamageType, DamageType] = {
+    "Fire": "Frost",
+    "Water": "Fire",
+    "Earth": "Water",
+    "Wind": "Earth",
+    "Lightning": "Wind",
+    "Frost": "Lightning"
+}
+
+def damage_modifier_for(attackerType: DamageType, defenderType: DamageType):
+    if attackerType not in DAMAGE_TYPE_BONUSES: return 0
+    if DAMAGE_TYPE_BONUSES[attackerType] == defenderType: return 1
+    if DAMAGE_TYPE_BONUSES[defenderType] == attackerType: return -1
+    return 0 
+
+class EnemyTemplate(object):
+    def __init__(self, name: str, base_hp: int, damage_type: str = None):
+        self.name = name
+        self.base_hp: int = base_hp
+        self.damage_type: str = damage_type
+    
+ENEMY_TEMPLATES: dict[str, EnemyTemplate] = {
+    "Zombie": EnemyTemplate("Zombie", 3, None)
+}
 
 class Enemy(object):
-    def __init__(self, name: str, hp: int, weak_to: set[str] = set(), strong_against: set[str] = set(), damage_type: str = None, drops: Dict[str, int] = None):
+    def __init__(self, template: EnemyTemplate, name: EnemyName):
+        self.template = template
         self.name = name
-        self.hp: int = hp
-        self.weak_to: set[str] = weak_to
-        self.strong_against: set[str] = strong_against
-        self.damage_type: str = damage_type
-        self.drops: Dict[str, int] = drops
+        self.hp = template.base_hp
 
-    def Instantiate(enemydef, name: str):
-        return Enemy(name, enemydef.hp, enemydef.weak_to, enemydef.strong_against, enemydef.damage_type, enemydef.drops)
-    
-    def take_hit(self, weapon: Weapon) -> int:
-        dmg: int = 2
-        damageType = weapon.damage_type
-        if damageType in self.weak_to:
-            dmg = 3
-        if damageType in self.strong_against:
-            dmg = 1
-        self.hp -= dmg
-        return dmg
+    def take_hit(self, type: DamageType):
+        self.hp -= 1 + damage_modifier_for(type, self.damage_type)
+
+    @property
+    def damage_type(self):
+        return self.template.damage_type
+
+    def __str__(self) -> str:
+        return "Enemy(template: " + self.template.name + ", name: " + self.name + ", hp: " + str(self.hp) + "/" + str(self.template.base_hp) + ")"
 
 class Decider(object):
-    def __init__(self):
-        """
-        Interface for the concept of selecting a weapon and targets during a turn
-        """
-    def select_weapon(weapons: list[Weapon], enemies: list[Enemy]) -> Weapon:
-        raise NotImplementedError()
-    def target_enemies(weapon: Weapon, enemies: dict[str, Enemy]) -> list[Enemy]:
+    """
+    Interface for the concept of selecting a weapon and targets during a turn
+    """
+    def choose_attack(self, player, remainingEnemies: list[Enemy]) -> tuple[EnemyName, DamageType]:
         raise NotImplementedError()
     
-class Decider_CLI(Decider):
-    def select_weapon(self, weapons: dict[str, Weapon], enemies: list[Enemy]) -> Weapon:
-        print("You have the following weapons:", list_str(weapons.keys()), "and the following enemies:", list_str([x.name for x in enemies if x.hp > 0]))
-        return ask_for_input(weapons, "You don't have a weapon called")
-    
-    def target_enemies(self, weapon: Weapon, enemies: dict[str, Enemy]) -> list[Enemy]:
-        print("You have", weapon.attacks_per_turn, "attacks available with", weapon.name, ", and the following potential targets:", list_str([x.name for x in enemies.values() if x.hp > 0]))
-        availableEnemies: list[Enemy] = [x for x in enemies.values() if x.hp > 0]
-        result: list[Enemy] = []
-        remaining_attacks = weapon.attacks_per_turn
-        while remaining_attacks > 0 and len(availableEnemies) > 0:
-            if len(availableEnemies) == 1:
-                result.append(availableEnemies[0])
-                break
-            print("You can attack the following enemies:", list_str([x.name for x in availableEnemies]))
-            enemy: Enemy = ask_for_input(enemies, "There isn't an enemy called")
-            result.append(enemy)
-            availableEnemies.remove(enemy)
-            remaining_attacks -= 1
-        return result
+class Decider_CLI(Decider):    
+    def choose_attack(self, player, remainingEnemies: list[Enemy]) -> tuple[EnemyName, DamageType]:
+        print("You have", player.remainingMoves, "remaining moves. Which enemy would you like to attack?", list_str(remainingEnemies))
+        choice: str = input()
+        while choice not in remainingEnemies:
+            print("That's not an available target.")
+            choice = input()
+        chosenEnemy: EnemyName = choice
+        remainingEnemies.remove(choice)
+        print("You have the following ammo available:")
+        for k, v in player.ammo:
+            print("\n",k,":",v)
+        print('You can also say "none" to attack without a damage type.')
+        choice: str = input()
+        while choice not in [x for x in player.ammo.keys() if player.ammo[x] > 0]:
+            print("That's not an available ammo type.")
+            choice = input()
+        chosenDamageType: DamageType = choice
+        return (chosenEnemy, chosenDamageType)
 
 class Player(object):
     # todo: genes determine these properties
     def __init__(self, decider: Decider):
         # how many hits the player can take until they die
         self.hp: int = 10
-        # how many times the player can use a weaapon
-        self.ammo: Dict[str, int] = {
-            "9mm": 5
-        }
-        # the weapons available to the player
-        self.weapons: dict[str, Weapon] = {
-            "knife": Weapon("knife", "blade"),
-            "gun": Weapon("gun", "bullet", "9mm", 2)
-        }
+        # how many times the player can use a damage type
+        self.ammo: dict[DamageType, int] = dict()
+        for k, _ in DAMAGE_TYPE_BONUSES:
+            self.ammo[k] = 5
         # the thing which decides how the player plays
         self.decider = decider
 
-    def attack(self, enemy: Enemy, weapon: Weapon):
-        if not weapon.has_ammo(self.ammo):
-            return
-        if weapon.required_ammo is not None:
-            self.ammo[weapon.required_ammo] -= 1
-            print("Using", weapon.name, "consumes 1", weapon.required_ammo, ", leaving you with", self.ammo[weapon.required_ammo])
-        result: int = enemy.take_hit(weapon)
-        print("Player deals", result, "damage to", enemy.name, "leaving them with", enemy.hp, "hp")
-        # todo: update epigenome with damage taken
-    
-    @property
-    def usable_weapons(self):
-        result = dict()
-        for k, v in self.weapons.items():
-            if v.has_ammo(self.ammo):
-                result[k] = v
-        return result
+    def do_attacks(self, enemies: dict[EnemyName, Enemy]) -> None:
+        remainingEnemies: list[EnemyName] = [x.name for x in enemies.values() if x.hp > 0]
+        self.remainingMoves: int = 3
+        while self.remainingMoves > 0 and any(remainingEnemies):
+            target, damageType = self.decider.choose_attack(self, remainingEnemies)
+            enemies[target].take_hit(damageType)
+            self.ammo[damageType] -= 1
 
+    def take_hit(self, damageType: DamageType) -> None:
+        self.hp -= damage_modifier_for(damageType, "Fire") # todo: player damage type weights based on genetics?
+
+    def __str__(self) -> str:
+        return "Player(hp: " + self.hp + ")"
 
 def battle(player: Player, enemies: dict[str, Enemy]) -> None:
     """
     A player fights enemies until either the player is dead or all the enemies are dead.
     """
     def do_turn():
-        weapon: Weapon = player.decider.select_weapon(player.usable_weapons, enemies.values())
-        targets: list[Enemy] = player.decider.target_enemies(weapon, enemies)
-        for target in targets:
-            player.attack(target, weapon)
-        # enemies each attack player once
+        player.do_attacks(enemies)
+        for enemy in [x for x in enemies.values() if x.hp > 0]:
+            player.take_hit(enemy.damage_type)
+
     print(player.hp)
     for enemy in enemies.values():
         print(enemy.hp)
@@ -145,10 +139,9 @@ def battle(player: Player, enemies: dict[str, Enemy]) -> None:
     # player heals hp?
 
 if __name__ == "__main__":
-    Zombie: Enemy = Enemy("Zombie", 5, "bullet")
     player: Player = Player(Decider_CLI())
-    enemies: dict[str, Enemy] = {}
+    enemies: dict[EnemyName, Enemy] = {}
     for i in range(2):
         name: str = "Zombie " + str(i + 1)
-        enemies[name] = Zombie.Instantiate(name)
+        enemies[name] = Enemy(ENEMY_TEMPLATES["Zombie"], name)
     battle(player, enemies)
