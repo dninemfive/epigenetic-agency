0671b578111993800351cf90e0961df7d9b1cdc7
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-08-24 16:19:42 -0700

    further gene stuff

--- model/gene.py
+++ model/gene.py
@@ -1,4 +1,5 @@
 from epigene import Epigene
+import random
 
 class GeneTemplate(object):
     def __init__(self, name: str):
@@ -15,6 +16,19 @@ GENE_TEMPLATES = [
 ]
 
 class Gene(object):
-    def __init__(self, template: GeneTemplate):
+    def __init__(self, template: GeneTemplate, weight: float = 0.5):
         self.template = template
-        self.epigene = Epigene
+        self.epigene = Epigene(self)    
+        self.weight = weight
+
+    @property
+    def name(self):
+        return self.template.name
+
+def cross(a: Gene, b: Gene, ratio: float = 0.5) -> Gene:
+    if a.name != b.name:
+        raise KeyError("Cannot cross two unrelated genes!")
+    return Gene(a.template, (ratio * a.weight) + ((1 - ratio) * b.weight))
+
+def mutate(gene: Gene, chance: float = 0.05, magnitude: float = 0.1) -> Gene:
+    pass
