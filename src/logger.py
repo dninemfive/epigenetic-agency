4a3ea4747b31367f98673325060807073ae6d893
dninemfive <11084010+dninemfive@users.noreply.github.com>
2023-08-27 09:00:53 -0700

    fix logger, seems to work

--- src/logger.py
+++ src/logger.py
@@ -1,18 +1,17 @@
 from typing import Any
 
-global default_indent
-default_indent: int = 0
-# items beyond this level of indent will __NOT__ be printed
-global max_indent
-max_indent: int = 1
+class Logger(object):
+    current_indent = 0
+    # items beyond this level of indent will __NOT__ be printed
+    max_indent = 0
 
 def increase_indent():
-    default_indent += 1
+    Logger.current_indent += 1
 
 def decrease_indent():
-    default_indent -= 1
+    Logger.current_indent -= 1
 
 def log(obj: Any, indent: int = None):
-    if indent is None: indent = default_indent
-    if indent > max_indent: return
+    if indent is None: indent = Logger.current_indent
+    if indent > Logger.max_indent: return
     print("  " * indent, obj, sep="")
